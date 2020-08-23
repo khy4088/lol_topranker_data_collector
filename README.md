34165989140189b9b12f000beb65b4840fd75cb7
PrestoLive <khy4088@gmail.com>
2020-08-23 23:26:17 +0900

    Readme 작성, visualization 작성중

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# lol_topranker_data_collector
+# League of Legends Top Ranker Data Collector
+
+
+
+- League of Legends API를 이용한 챌린저~마스터 유저의 데이터를 수집하여 데이터화 하는 코드
+- 수집된 코드는 csv로 저장됨
+
--- visualization.py
+++ visualization.py
@@ -3,12 +3,28 @@ import pandas as pd
 import numpy as np
 
 data = pd.read_csv('result.csv')
-bins = np.arange(43.5,88.1,0.5) #https://hashcode.co.kr/questions/646/range%EC%9D%98-step%EC%97%90%EC%84%9C-%EC%A0%95%EC%88%98-%EB%A7%90%EA%B3%A0-float%EC%9D%84-%EC%93%B0%EB%A0%A4%EB%A9%B4
-
-cnt = data['win_rate'].value_counts(bins=bins, sort=False) #bins범위마다 체크.
+bins = np.arange(43.5,88.0,0.5) #https://hashcode.co.kr/questions/646/range%EC%9D%98-step%EC%97%90%EC%84%9C-%EC%A0%95%EC%88%98-%EB%A7%90%EA%B3%A0-float%EC%9D%84-%EC%93%B0%EB%A0%A4%EB%A9%B4
+bins_1 = np.arange(43.5, 88.1,0.5)
+cnt = data['win_rate'].value_counts(bins=bins_1, sort=False) #bins범위마다 체크.
 
 cnt_list = cnt.values.tolist()  #https://note.nkmk.me/en/python-pandas-list/
 bins = bins.tolist()
 
-print(bins.type())
-print(cnt_list.type())
+print(type(bins))
+print(type(cnt_list))
+
+print(len(bins))
+print(len(cnt_list))
+
+
+plt.bar(bins, cnt_list)
+
+plt.title('승률 분포', fontsize=20)
+
+plt.xlabel('Winrate', fontsize=18)
+
+plt.ylabel('User', fontsize=18)
+
+plt.xticks(bins, cnt_list, fontsize=15)
+
+plt.show()
