127c2075587170b0bd14f4c646df00f45c4a3a72
PrestoLive <khy4088@gmail.com>
2020-08-27 22:50:38 +0900

    json에서 받아온 데이터를 csv로 변환 (완료)

--- get_champion_data.py
+++ get_champion_data.py
@@ -2,6 +2,7 @@ import requests
 import numpy as np
 import pandas as pd
 import json
+import csv
 
 with open('./champion_ko_KR.json', 'r', encoding='UTF-8') as f:
     json_dict = json.load(f)
@@ -13,4 +14,18 @@ champ_list = []
 for value in champ_data.keys():
     champ_list.append(value)
 
-print(champ_list)
+column = list(champ_data[champ_list[0]].keys())
+print(column)
+
+csv_data = pd.DataFrame(columns=column)
+print(csv_data)
+csv_data.to_csv('champ_data.csv')
+
+f = open("./champ_data.csv", 'w', encoding='UTF-8')
+writer = csv.writer(f)
+
+writer.writerow(column)
+for i in range(len(champ_list)):
+    writer.writerow(list(champ_data[champ_list[i]].values()))
+
+f.close()
