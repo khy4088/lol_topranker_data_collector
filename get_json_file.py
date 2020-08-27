ac9b25419e86ae8c95aafaf5b9793f7c27ecbf77
PrestoLive <khy4088@gmail.com>
2020-08-27 23:11:45 +0900

    json 파일 가져오기

---
+++ get_json_file.py
@@ -0,0 +1,17 @@
+from urllib import request
+
+champion_en_US = "champion_en_US.csv"
+champion_ko_KR = "champion_ko_KR.csv"
+en_US_data = "http://ddragon.leagueoflegends.com/cdn/10.16.1/data/en_US/champion.json"
+ko_KR_data = "http://ddragon.leagueoflegends.com/cdn/10.16.1/data/ko_KR/champion.json"
+
+usdata = request.urlopen(en_US_data).read()
+krdata = request.urlopen(ko_KR_data).read()
+
+with open(usdata, mode="wb") as f:
+    f.write(usdata)
+    print("저장되었습니다.")
+
+with open(krdata, mode="wb") as f:
+    f.write(usdata)
+    print("저장되었습니다.")
